246c49ea4529782d3313ab23da98543798b86179
heartOfblack <heartOfblack@163.com>
2017-09-08 11:10:45 +0800

    新增参数join，判断如果相同参数是拼接还是替换

--- js/Tool.js
+++ js/Tool.js
@@ -1,15 +1,26 @@
 $.fn.extend({
-			/**
-			 * @param {Object} 表单以外的额外数据
-			 */
-			formTojson: function(newObj) {
-				$t = $(this);
-				var obj = {};
-				$.each($($t).serializeArray(), function(i, _obj) {
-						obj[_obj.name] = _obj.value;
-				});
-				return $.extend(true, obj , newObj);
-			}
+			
+				/**
+				 * @param {Object} join 遇到相同的参数是否合并，合并用逗号隔开
+				 * @param {Object} newObj 表单外的参数
+				 */
+				formTojson: function(join,newObj) {
+					arguments[0]===undefined?false:true;
+					$t = $(this);
+					var obj = {};
+					$.each($($t).serializeArray(), function(i, _obj) {
+
+						if(obj.hasOwnProperty(_obj.name) && join) //如果已经存在，则根据条件选择是去除还是拼接在后面
+						{
+							obj[_obj.name]+= ','+_obj.value;	
+						} else {
+							obj[_obj.name] = _obj.value;
+						}
+					});
+					return $.extend(true, obj, newObj);
+				}
+
+		
 		})
 
 
