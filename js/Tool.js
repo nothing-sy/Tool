f8be74dade6b7dee14d91ef7c4263fd2bf2760d7
heartOfblack <heartOfblack@163.com>
2017-09-10 17:37:02 +0800

    新增设置input类型为日期类型控件为当前日期的方法$.fn.SetCurDateById，只能通过有ID的设置默认值;新增检查日期有效范围，$.checkDate(from,to)第一个日期必须在第二个日期之前或一致

--- js/Tool.js
+++ js/Tool.js
@@ -1,26 +1,43 @@
 $.fn.extend({
-			
-				/**
-				 * @param {Object} join 遇到相同的参数是否合并，合并用逗号隔开
-				 * @param {Object} newObj 表单外的参数
-				 */
-				formTojson: function(join,newObj) {
-					arguments[0]===undefined?false:true;
-					$t = $(this);
-					var obj = {};
-					$.each($($t).serializeArray(), function(i, _obj) {
 
-						if(obj.hasOwnProperty(_obj.name) && join) //如果已经存在，则根据条件选择是去除还是拼接在后面
-						{
-							obj[_obj.name]+= ','+_obj.value;	
-						} else {
-							obj[_obj.name] = _obj.value;
-						}
-					});
-					return $.extend(true, obj, newObj);
-				}
+	/**
+	 * @param {Object} join 遇到相同的参数是否合并，合并用逗号隔开
+	 * @param {Object} newObj 表单外的参数
+	 */
+	formTojson: function(join, newObj) {
+		arguments[0] === undefined ? false : true;
+		var obj = {};
+		$.each($(this).serializeArray(), function(i, _obj) {
 
-		
-		})
+			if(obj.hasOwnProperty(_obj.name) && join) //如果已经存在，则根据条件选择是去除还是拼接在后面
+			{
+				obj[_obj.name] += ',' + _obj.value;
+			} else {
+				obj[_obj.name] = _obj.value;
+			}
+		});
+		return $.extend(true, obj, newObj);
+	},
+	setCurDateById: function() {
+		$.each($(this), function(i) {
+			document.getElementById($(this).attr('id')).valueAsDate = new Date();
+		});
 
+	}
 
+});
+
+$.extend({
+	checkDate: function(from, to) {
+		var check_ok = true;
+		var oDate1 = new Date(from);
+		var oDate2 = new Date(to);
+		if(oDate1.getTime() > oDate2.getTime()) {
+
+			check_ok = false;
+		}
+
+		return check_ok;
+	}
+
+});
